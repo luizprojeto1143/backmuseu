ea41a78ea976ca62ce7124fb6759f6618d3a9baa
luizprojeto1143 <fwegwrghe8@gmail.com>
2025-11-24 11:56:55 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import "reflect-metadata";
 import dotenv from "dotenv";
 dotenv.config();
+
 import { AppDataSource } from "./utils/data-source";
 import app from "./app";
 
 const PORT = process.env.PORT || 3000;
 
-AppDataSource.initialize().then(async () => { const {{ ensureMasterUser }} = await import('./utils/master-seeder'); await ensureMasterUser(); })
-  .then(() => {
+AppDataSource.initialize()
+  .then(async () => {
+    // Import dinâmico correto do seeder
+    const { ensureMasterUser } = await import("./utils/master-seeder");
+    await ensureMasterUser();
+
     console.log("📦 Conectado ao banco de dados");
+
     app.listen(PORT, () => {
       console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
     });
